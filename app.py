21028dbaaa75857d10ce2f5ed86dccb39b2a845c
jia0224 <jenny40922@gmail.com>
2025-11-14 14:36:36 +0800

    update: add return for Qbi

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ import os
 app = Flask(__name__)
 
 @app.route("/api/qbi/test", methods=["POST"])
+
 def qbi_test():
     data = request.get_json(force=True, silent=True) or {}
-    user_input = data.get("ask_input", "")
+    user_input = data.get("ask_input", "") or request.args.get("ask_input", "")
     print("Qbi 傳來的內容：", user_input)
+
+    return jsonify({
+        "text": [f"我收到你的內容：{user_input}"],
+        "type": "Text"
+    })
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
